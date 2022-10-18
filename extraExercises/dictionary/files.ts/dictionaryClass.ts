49e09830baf63f1d401126df521562911880db37
francovelazquez2011 <francovelazquez2011@gmail.com>
2022-10-18 19:42:06 -0300

    Trabajando

--- extraExercises/dictionary/files.ts/dictionaryClass.ts
+++
@@ -1,9 +0,0 @@
-import { Word } from "./wordClass";
-
-export class Dictionary {
-    private wordList:Word[];
-
-    public constructor(pWord:Word[]){
-        this.wordList=pWord.sort();
-    }
-}
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { Cliente } from "./claseCliente";
+import { Revista } from "./claseRevista";
+import { Libro } from "./claseLibro";
+import { Libreria } from "./claseLibreria";
+
